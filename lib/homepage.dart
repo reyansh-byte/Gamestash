0aa3365ddd504abe6694a24644c4f16616a7a051
Reyansh-DUB <103351603+Reyansh-DUB@users.noreply.github.com>
2025-07-13 14:23:43 +0530

    Homepage commit

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,5 +1,6 @@
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -10,105 +11,98 @@ class Homepage extends StatefulWidget {
 
 class _HomepageState extends State<Homepage> {
   int _currentIndex = 0;
+
+  final List<_NavItemData> _navItems = [
+    _NavItemData('assets/home.png', 'Home'),
+    _NavItemData('assets/compass.png', 'Discover'),
+    _NavItemData('assets/game.png', 'Games'),
+    _NavItemData('assets/profile-candidate.png', 'Profile'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      bottomNavigationBar: CurvedNavigationBar(
-        backgroundColor: Colors.black,
-        color: Colors.grey[900]!,
-        buttonBackgroundColor: Colors.green,
-        height: 70,
-        index: _currentIndex,
-        items: <Widget>[
-          Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Image.asset(
-                'assets/home.png',
-                width: 24,
-                height: 24,
-                color: _currentIndex == 0 ? Colors.black : Colors.white,
-              ),
-              SizedBox(height: 4),
-              Text(
-                'Home',
-                style: TextStyle(
-                  color: _currentIndex == 0 ? Colors.black : Colors.white,
-                  fontSize: 10,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-            ],
+      bottomNavigationBar: Stack(
+        children: [
+          CurvedNavigationBar(
+            height: 70,
+            index: _currentIndex,
+            backgroundColor: Colors.black,
+            color: Colors.grey[900]!,
+            buttonBackgroundColor: Colors.transparent,
+            items: List.generate(_navItems.length, (index) {
+              final isSelected = _currentIndex == index;
+
+              if (isSelected) {
+                return Container(
+                  width: 54,
+                  height: 54,
+                  decoration: const BoxDecoration(
+                    color: Color(0xFF00C851),
+                    shape: BoxShape.circle,
+                  ),
+                  child: Center(
+                    child: Image.asset(
+                      _navItems[index].iconPath,
+                      width: 35,
+                      height: 35,
+                      color: Colors.black,
+                    ),
+                  ),
+                );
+              } else {
+                return Image.asset(
+                  _navItems[index].iconPath,
+                  width: 35,
+                  height: 35,
+                  color: Colors.white,
+                );
+              }
+            }),
+            onTap: (index) {
+              setState(() {
+                _currentIndex = index;
+              });
+            },
           ),
-          Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Image.asset(
-                'assets/compass.png',
-                width: 24,
-                height: 24,
-                color: _currentIndex == 1 ? Colors.black : Colors.white,
+          // Text labels overlay
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              height: 70,
+              child: Row(
+                children: List.generate(_navItems.length, (index) {
+                  final isSelected = _currentIndex == index;
+                  return Expanded(
+                    child: Container(
+                      alignment: Alignment.bottomCenter,
+                      padding: const EdgeInsets.only(bottom: 8),
+                      child: Text(
+                        _navItems[index].label,
+                        style: GoogleFonts.orbitron(
+                          color: isSelected ? Color(0xFF00C851) : Colors.white,
+                          fontSize: 12,
+                          fontWeight: isSelected ? FontWeight.bold : FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  );
+                }),
               ),
-              SizedBox(height: 4),
-              Text(
-                'Discover',
-                style: TextStyle(
-                  color: _currentIndex == 1 ? Colors.black : Colors.white,
-                  fontSize: 10,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-            ],
-          ),
-          Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Image.asset(
-                'assets/game.png',
-                width: 24,
-                height: 24,
-                color: _currentIndex == 2 ? Colors.black : Colors.white,
-              ),
-              SizedBox(height: 4),
-              Text(
-                'Games',
-                style: TextStyle(
-                  color: _currentIndex == 2 ? Colors.black : Colors.white,
-                  fontSize: 10,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-            ],
-          ),
-          Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Image.asset(
-                'assets/profile_candidate.png',
-                width: 24,
-                height: 24,
-                color: _currentIndex == 3 ? Colors.black : Colors.white,
-              ),
-              SizedBox(height: 4),
-              Text(
-                'Profile',
-                style: TextStyle(
-                  color: _currentIndex == 3 ? Colors.black : Colors.white,
-                  fontSize: 10,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-            ],
+            ),
           ),
         ],
-        onTap: (index) {
-          setState(() {
-            _currentIndex = index;
-          });
-          print('Tapped index: $index');
-        },
       ),
     );
   }
+}
+
+class _NavItemData {
+  final String iconPath;
+  final String label;
+
+  const _NavItemData(this.iconPath, this.label);
 }
